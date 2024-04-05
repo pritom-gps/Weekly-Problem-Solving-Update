1a51bf33207e338f52039bccb254c8f0e9c36023
Pritom Das <150539681+pritom-gps@users.noreply.github.com>
2024-04-05 21:23:55 +0530

    Create B - OR Permutation.cpp

---
+++ Week 4/Day 1/B - OR Permutation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tst;
+    cin >> tst;
+    while (tst--)
+    {
+        int n;
+        cin >> n;
+        for (int i = n; i >= 1; i--)
+            cout << i << " ";
+        cout << '\n';
+    }
+    return 0;
+}
